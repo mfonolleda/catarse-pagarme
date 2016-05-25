83ff2d25c694502d57e6c437ac1b2d83c72982e4
Miquel Fonolleda <miquel@albira.com>
2016-05-25 11:20:02 +0000

    update to fix precompilation of assets

--- app/assets/javascripts/catarse_pagarme/application.js
+++ app/assets/javascripts/catarse_pagarme/application.js
@@ -1,12 +1,7 @@
-App.addChild('Pagarme', {
-  el: '#catarse_pagarme_form',
+//= require ./app/app
+//= require_tree ./app
 
-  activate: function() {
-    this.message = this.$('.next_step_after_valid_document .alert-danger');
-    this.loader = this.$('.loader img');
-
-    this.contributionId = $('input#contribution_id').val();
-    this.projectId = $('input#project_id').val();
-  }
-});
+$(function(){
+  app.createViewGetters();
+  });
 
